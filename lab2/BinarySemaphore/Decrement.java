60ea838f0edb658770fccef2410222800d19b845
grzegorzborkowski <gb.grzegorzborkowski@gmail.com>
2016-03-09 14:06:53 +0100

    add .gitignore file

--- lab2/BinarySemaphore/Decrement.java
+++ lab2/BinarySemaphore/Decrement.java
@@ -1,16 +1,20 @@
 class Decrement implements Runnable {
 
+    BinarySemaphre binarySemaphore;
     PackedInteger packedInteger;
     int range;
 
-    public Decrement(PackedInteger packedInteger, int range) {
+    public Decrement(BinarySemaphore binarySemaphore, PackedInteger packedInteger, int range) {
+        this.binarySemaphre = binarySemaphore;
         this.packedInteger = packedInteger;
         this.range = range;
     }
 
     public void run() {
         for (int i = 0; i < range; i++) {
+            binarySemaphore.p();
             packedInteger.decrease();
+            binarySemaphore.v();
         }
         packedInteger.printValue();
     }
